a15d00f390b4f4e14b1cddc9ddac22d3bbb40ed5
KsanaSal <oasalivon@gmail.com>
2022-10-25 16:53:34 +0300

    Solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,130 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackButton } from './FeedbackButtons/FeedbackButtons';
 import { Notification } from './Notification/Notification';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const changeState = type => {
+    switch (type) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  changeState = type => {
-    this.setState(prevState => {
-      return { [type]: prevState[type] + 1 };
-    });
+  const countTotalFeedback = () => {
+    return bad + good + neutral;
   };
 
-  countTotalFeedback = () => {
-    return this.state.bad + this.state.good + this.state.neutral;
+  const countPositiveFeedbackPercentage = total => {
+    return Math.round((good * 100) / total);
   };
 
-  countPositiveFeedbackPercentage = total => {
-    return Math.round((this.state.good * 100) / total);
-  };
+  const options = ['good', 'neutral', 'bad'];
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          paddingLeft: 30,
-          fontSize: 30,
-          fontWeight: 400,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feetback">
-          <FeedbackButton
-            changeState={this.changeState}
-            options={Object.keys(this.state)}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        paddingLeft: 30,
+        fontSize: 30,
+        fontWeight: 400,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feetback">
+        <FeedbackButton changeState={changeState} options={options} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positiveFeedback={countPositiveFeedbackPercentage(
+              countTotalFeedback()
+            )}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() ? (
-            <Statistics
-              state={this.state}
-              total={this.countTotalFeedback()}
-              positiveFeedback={this.countPositiveFeedbackPercentage(
-                this.countTotalFeedback()
-              )}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   changeState = type => {
+//     this.setState(prevState => {
+//       return { [type]: prevState[type] + 1 };
+//     });
+//   };
+
+//   countTotalFeedback = () => {
+//     return this.state.bad + this.state.good + this.state.neutral;
+//   };
+
+//   countPositiveFeedbackPercentage = total => {
+//     return Math.round((this.state.good * 100) / total);
+//   };
+
+//   render() {
+//     return (
+//       <div
+//         style={{
+//           height: '100vh',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           alignItems: 'flex-start',
+//           paddingLeft: 30,
+//           fontSize: 30,
+//           fontWeight: 400,
+//           color: '#010101',
+//         }}
+//       >
+//         <Section title="Please leave feetback">
+//           <FeedbackButton
+//             changeState={this.changeState}
+//             options={Object.keys(this.state)}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() ? (
+//             <Statistics
+//               state={this.state}
+//               total={this.countTotalFeedback()}
+//               positiveFeedback={this.countPositiveFeedbackPercentage(
+//                 this.countTotalFeedback()
+//               )}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import { List, ListTitle } from './Statistics.styled';
 
-export const Statistics = ({ state, total, positiveFeedback }) => {
+export const Statistics = ({ good, neutral, bad, total, positiveFeedback }) => {
   return (
     <List>
-      <ListTitle>Good: {state.good}</ListTitle>
-      <ListTitle>Neutral: {state.neutral}</ListTitle>
-      <ListTitle>Bad: {state.bad}</ListTitle>
+      <ListTitle>Good: {good}</ListTitle>
+      <ListTitle>Neutral: {neutral}</ListTitle>
+      <ListTitle>Bad: {bad}</ListTitle>
       <ListTitle>Total: {total}</ListTitle>
       <ListTitle>Positive feedback: {positiveFeedback}%</ListTitle>
     </List>
@@ -16,9 +16,7 @@ export const Statistics = ({ state, total, positiveFeedback }) => {
 Statistics.propTypes = {
   total: PropTypes.number.isRequired,
   positiveFeedback: PropTypes.number.isRequired,
-  state: PropTypes.exact({
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-  }),
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
 };
